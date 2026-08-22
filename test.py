fe8549d7eecba5ba2b263660fae86a4b1c67a2b2
23csb27 <23csb27@gmail.com>
2026-08-22 18:27:33 +0530

    tavilyand flight tools added

---
+++ test.py
@@ -0,0 +1,6 @@
+from tools.tavily_tool import tavily_search
+from tools.flight_tool import search_flights
+#res = tavily_search("Best hotels in India")
+#print(res)
+res=search_flights("plan a 7 days Nepal trip from Bangaladesh")
+print(res)
